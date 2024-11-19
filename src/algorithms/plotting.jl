a102efb2a31f88eb01ebfd1dc9deda434536e6ee
Ahmed Mogahed <a.mogahed@inginne.com>
2024-11-20 00:11:30 +0200

    Supporting animations

--- src/algorithms/plotting.jl
+++ src/algorithms/plotting.jl
@@ -1,12 +1,27 @@
 module Plotting
-	
+
 	using Plots
 	
-	export plot_positions_from_iteration
+	export plot_positions_from_iteration,build_position_animation_from_iteration
 
 	# TODO: generalize (dependent on position indices)
 	function plot_positions_from_iteration(iteration;xlims=nothing,ylims=nothing)
-		x, _ = iteration
+		p = plot_positions_from_iteration_as_plot(iteration[1]; xlims=xlims, ylims=ylims)
+		gui(p)
+		println("Press Enter to close the plot window...")
+		readline()
+	end
+
+	function build_position_animation_from_iteration(iteration; file)
+		anim = @animate for i ∈ 1:length(iteration[1])
+		    plot_positions_from_iteration_as_plot(iteration[1][1:i])
+		end
+		gif(anim, file, fps = 15)
+	end
+
+	# TODO: generalize (dependent on position indices)
+	function plot_positions_from_iteration_as_plot(state_iteration;xlims=nothing,ylims=nothing)
+		x = state_iteration
 		posx1, posy1, posx2, posy2, posx3, posy3 = map(pos->getindex.(x,pos),[1,2,5,6,9,10])
 
 		# Plot with optional xlims and ylims
@@ -18,13 +33,10 @@ module Plotting
 	        plot_args[:ylims] = ylims
 	    end
 
-		plot!(posx1,posy1, plot_args...)
+		p = plot(posx1,posy1, plot_args...)
 		plot!(posx2,posy2, plot_args...)
 		plot!(posx3,posy3, plot_args...)
 
-		gui()
-		println("Press Enter to close the plot window...")
-		readline()
+		return p
 	end
-
 end
