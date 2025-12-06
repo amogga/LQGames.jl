23b8d38ab197ef0be34995f336bd108ff31cfb80
Ahmed Mogahed <a.mogahed@inginne.com>
2025-12-06 23:49:44 +0200

    Add benchmarking

---
+++ src/examples/benchmarking.jl
@@ -0,0 +1,20 @@
+include("example1/example1.jl")
+include("example1/objective_function.jl")
+include("example1/players.jl")
+include("../solver/lq_game.jl")
+include("../structs/game_state.jl")
+include("../algorithms/plotting.jl")
+using .Example1
+using .LQGameSolver
+using .Plotting
+using BenchmarkTools
+using ForwardDiff
+using LinearAlgebra
+
+
+states = [6.5, 0.0, pi / 2.0 , 1.0, 1.5, 40, -(pi / 2.0), 0.1, 0.0, 22.0, 0.0, 2.0]
+input = [0,0,0,0,0,0.0]
+
+solving_time = @benchmark overall_solver(states,input; iterations_count=55, sample=0.25, maxtime=5)
+
+display(solving_time)
